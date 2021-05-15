b1bd5974b2593e7ae2eb274b04641631948eb12e
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-05-15 10:39:34 +0530

    floyd's algo (hashing can also be used)

---
+++ linked_list/detect_and_remove_loop.py
@@ -0,0 +1,28 @@
+def detectAndRemoveLoop(head):
+     
+    if (head == None or head.next == None):
+        return None
+  
+    slow = head
+    fast = head
+  
+    slow = slow.next
+    fast = fast.next.next
+  
+    while (fast and fast.next):
+        if (slow == fast):
+            break
+         
+        slow = slow.next
+        fast = fast.next.next
+  
+    if (slow != fast):
+        return None
+  
+    slow = head
+     
+    while (slow != fast):
+        slow = slow.next
+        fast = fast.next
+  
+    return slow
