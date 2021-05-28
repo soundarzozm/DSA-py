0363136f8d02eab2161c70aef7e614ea8d7373cc
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-05-28 17:44:14 +0530

    aditya verma god

---
+++ dynamic_programming/0-1_knapsack/count_of_subsets_with_given_sum.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def subsetSum(wt, n, W):
+
+    for i in range(n+1):
+        for j in range(sum+1):
+            if i==0:
+                t[i][j]=0
+            elif j==0:
+                t[i][j]=1
+
+    for i in range(1, n+1):
+        for j in range(1, W+1):
+
+            if wt[i-1]<=j:
+                t[i][j] = (t[i-1][j-wt[i-1]]) + (t[i-1][j])
+
+            else:
+                t[i][j] = t[i-1][j]
+
+    return t[n][W]
+
+
+if __name__=="__main__":
+    arr = [2, 3, 7, 8, 10]
+    sum = 15
+    n = 5
+
+    t = [[None] * (sum+1)] * (n+1)
+
+    t = np.array(t)
+
+    print(subsetSum(arr, n, sum))
+ 
