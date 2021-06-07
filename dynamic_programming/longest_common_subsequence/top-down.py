29c3d6d6e449146170ef3ff49d1e29e64552984c
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-06-07 15:56:03 +0530

    clarified

---
+++ dynamic_programming/longest_common_subsequence/top-down.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def LCS(x, y, m, n):
+
+    #Base Case
+    for i in range(m+1):
+        for j in range(n+1):
+            if i==0 or j==0:
+                t[i][j] = 0
+    
+    #Choice Diagram
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if x[i-1]==y[j-1]:
+                t[i][j] = 1 + t[i-1][j-1]
+            else:
+                t[i][j] = max(t[i-1][j], t[i][j-1])
+
+    return t[m][n]
+
+if __name__=="__main__":
+
+    x = ""
+    m = len(x)
+
+    y = ""
+    n = len(y)
+
+    #Initialization
+    t = [[None] * (m+1)] * (n+1)
+    t = np.array(t)
+
+    print(LCS(x, y, m, n))
