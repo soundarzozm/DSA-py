50d7ad3435d76009a57d4c17d9202b8646c61e06
Soundar Murugan <soundar.murugan1@learner.manipal.edu>
2021-06-10 18:20:53 +0530

    bfs for single component

--- graph/adjacency_list.py
+++ graph/adjacency_list.py
@@ -26,10 +26,38 @@ class Graph:
     def degree(self, node):
         return len(self.adjList[node])
 
+    def bfs(self, node):
+        bfs_arr = []
+        visited = {}
+
+        for i in self.nodes:
+            visited[i]=False
+
+        queue = []
+
+        queue.append(node)
+        visited[node] = True
+
+        while queue:
+
+            node = queue.pop(0)
+            bfs_arr.append(node)
+
+            for i in self.adjList[node]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+        return bfs_arr
+
 if __name__=="__main__":
 
     graph = Graph()
 
-    graph.addEdge("Chennai", "Bangalore")
-    graph.printAdjList()
+    graph.addEdge(1, 2)
+    graph.addEdge(7, 5)
+    graph.addEdge(2, 7)
+    graph.addEdge(2, 3)
+    graph.addEdge(3, 5)
+    graph.addEdge(4, 6)
 
+    print(graph.bfs(1))
