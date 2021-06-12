8d10ae81af0eabad1d1d4227030b049d90e5751e
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-06-12 17:11:58 +0530

    works

---
+++ dynamic_programming/matrix_chain_multiplication/scrambled_string_recursive.py
@@ -0,0 +1,39 @@
+def scrambledString(a, b):
+
+    #Base condition
+    if a==b:
+        return True
+    if len(a)<=1 or len(b)<=1:
+        return False
+
+    #Initialization
+    n = len(a)
+    flag = False
+    flag1 = False
+    flag2 = False
+
+    #Loop
+    for i in range(1, n):
+        if (scrambledString(a[:i], b[-i:]) & scrambledString(a[i:], b[:-i])):
+            flag1 = True
+        if (scrambledString(a[:i], b[:i]) & scrambledString(a[i:], b[i:])):
+            flag2 = True
+        if flag1 or flag2:
+            flag = True
+            break
+    return flag
+
+if __name__ == "__main__":
+    
+    a = "great"
+    b = "rgeat"
+
+    if len(a)!=len(b):
+        print(False)
+        exit()
+
+    if a=="" and b=="":
+        print(True)
+        exit()
+
+    print(scrambledString(a, b))
