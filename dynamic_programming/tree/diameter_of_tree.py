63789d6387f4f1a82ad960a81a169568d8d83c46
Soundar Murugan <soundar.murugan1@learner.manipal.edu>
2021-06-13 18:52:19 +0530

    works

--- dynamic_programming/tree/diameter_of_tree.py
+++ dynamic_programming/tree/diameter_of_tree.py
@@ -9,12 +9,12 @@ def solve(root, result):
     right = solve(root.right, result)
 
     #Induction
-    i_am_root = 1 + max(left, right)
-    i_am_not_root = 1 + left + right
+    i_am_not_root = 1 + max(left, right)
+    i_am_root = 1 + left + right
     ans = max(i_am_root, i_am_not_root)
     result = max(ans, result)
 
-    return i_am_root
+    return i_am_not_root
 
 if __name__ == "__main__":
 
