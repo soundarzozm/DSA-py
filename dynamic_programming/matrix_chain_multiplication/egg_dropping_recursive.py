c97b5c956e8066e3fa904f90186b46f91cbce515
Soundar Murugan <soundar.murugan1@learner.manipal.edu>
2021-06-13 12:51:20 +0530

    rectified recursive and memoized

--- dynamic_programming/matrix_chain_multiplication/egg_dropping_recursive.py
+++ dynamic_programming/matrix_chain_multiplication/egg_dropping_recursive.py
@@ -3,8 +3,6 @@ import sys
 def eggDropping(e, f):
 
     #Initialization
-    if t[e][f]!=-1:
-        return t[e][f]
     mn = INT_MAX
 
     #Base case
@@ -15,27 +13,14 @@ def eggDropping(e, f):
 
     #Loop
     for k in range(1, f+1):
-
-        if t[e-1][k-1]!=-1:
-            egg_break = t[e-1][k-1]
-        else:
-            egg_break = eggDropping(e-1, k-1)
-
-        if t[e][f-k]!=-1:
-            egg_not_break = t[e][f-k]
-        else:
-            egg_not_break = eggDropping(e, f-k)
-
-        temp = 1 + max(egg_break, egg_not_break)
+        temp = 1 + max(eggDropping(e-1, k-1),  eggDropping(e, f-k))
         mn = min(mn, temp)
-    t[e][f] = mn
-    return t[e][f]
+    return mn
 
 if __name__=="__main__":
     f = 4
     e = 2
 
     INT_MAX = sys.maxsize
-    t = [[-1 for i in range(f+1)] for j in range(e+1)]
 
     print(eggDropping(e, f))
