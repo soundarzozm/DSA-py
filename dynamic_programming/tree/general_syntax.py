f959aec945d8dc2db921067f411820c692ca214c
Soundar Murugan <soundar.murugan1@learner.manipal.edu>
2021-06-13 18:46:03 +0530

    variable names updated

--- dynamic_programming/tree/general_syntax.py
+++ dynamic_programming/tree/general_syntax.py
@@ -9,12 +9,12 @@ def solve(root, result):
     right = solve(root.right, result)
 
     #Induction
-    temp_pass = 1 + max(left, right)
-    temp_through = 1 + left + right
-    ans = max(temp_pass, temp_through)
+    i_am_root = 1 + max(left, right)
+    i_am_not_root = 1 + left + right
+    ans = max(i_am_root, i_am_not_root)
     result = max(ans, result)
 
-    return temp_pass
+    return i_am_root
 
 if __name__ == "__main__":
 
