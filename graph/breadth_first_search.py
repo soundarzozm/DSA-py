a829329c968b18e33474221bffb33475617fb00c
Soundar Murugan <soundar.murugan1@learner.manipal.edu>
2021-06-14 17:05:07 +0530

    modified test case

--- graph/breadth_first_search.py
+++ graph/breadth_first_search.py
@@ -34,10 +34,10 @@ if __name__=="__main__":
     graph = Graph()
 
     graph.addEdge(1, 2)
-    graph.addEdge(7, 5)
-    graph.addEdge(2, 7)
-    graph.addEdge(2, 3)
-    graph.addEdge(3, 5)
+    graph.addEdge(2, 4)
     graph.addEdge(4, 6)
+    graph.addEdge(6, 7)
+    graph.addEdge(7, 2)
+    graph.addEdge(3, 5)
 
     print(bfs(graph, 5))
