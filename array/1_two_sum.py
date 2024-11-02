bebca1e9f4fba70c5a5966f51d2664c5e80a0b5e
soundarzozm <soundarmurugan91@gmail.com>
2024-11-02 17:58:25 +0530

    added comments for better readability

--- array/1_two_sum.py
+++ array/1_two_sum.py
@@ -1,16 +1,29 @@
 class Solution:
     def twoSum(self, nums, target):
+
+	# Initialize a hashmap since we need to run this algorithm in O(n) time
         hashMap = {}
         length = len(nums)
-
+	
+	# Populate the hashmap with all values and their indices in an array
+	# For example: nums = [1, 2, 3, 1]
+	# {
+	# 	1: [0, 3],
+	#	2: [1],
+	# 	3: [2]
+	# }
 	for i in range(l):
             if nums[i] not in hashMap:
                 hashMap[nums[i]] = []
             hashMap[nums[i]].append(i)
-
+	
         for i in range(l):
-            if target-nums[i] in hashMap:
+        # If target - nums[i] is present in the hashmap, we have our answer. 
+	# But what if nums[i] == target - nums[i]? We might be referring to the same element twice.
+	    if target-nums[i] in hashMap:
                 if nums[i] == target-nums[i]:
+		
+		    # Here we make sure we are not referring to the same element twice
                     if (len(hashMap[nums[i]]) >= 2):
                         return hashMap[nums[i]][:2]
                     continue
