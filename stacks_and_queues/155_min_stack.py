bc744ac4abd955a901d4f5190e9e43a908144710
soundarzozm <soundarmurugan91@gmail.com>
2024-11-26 18:37:26 +0530

    Added minstack comments

--- stacks_and_queues/155_min_stack.py
+++ stacks_and_queues/155_min_stack.py
@@ -1,25 +1,24 @@
 class MinStack:
+# What is a min stack? What is a monotonically decreasing stack? They're the same
+# The elements in these data structures are in decreasing order only.
+# So, the top element will logically be the smallest
+
     def __init__(self):
+        # We are going to use a standard stack
+        # But instead of adding elements directly, we add a tuple (element, min. element till now)
         self.stack = []
-        self.minStack = []
 
     def push(self, val):
-        self.stack.append(val)
-
-        if self.minStack:
-            self.minStack.append(min(val, self.minStack[-1]))
+        if self.stack:
+            self.stack.append((val, min(val, self.stack[-1][1])))
         else:
-            self.minStack.append(val)
+            self.stack.append((val, val))
 
     def pop(self):
         self.stack.pop()
-        self.minStack.pop()
 
     def top(self):
-        return self.stack[-1]
+        return self.stack[-1][0]
 
     def getMin(self):
-        return self.minStack[-1]
-
-    def printStack(self):
-        print(self.stack)
+        return self.stack[-1][1]
